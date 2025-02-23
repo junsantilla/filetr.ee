2514bcc5fac75fdcf5333c0da596cc86db029b47
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-02-24 01:15:35 +0800

    Create Express.js File Structure
    Changes:
    - Create Express.js page.
    - Separate the related data such as file structures and file structure explanation.
    - Refactor structure-template.tsx by adding conditional rendering when certain tab (e.g basic, intermediate, advanced) is not yet available/provided.

---
+++ app/express-js/page.tsx
@@ -0,0 +1,81 @@
+import { StructureTemplate } from "@/components/structure-template";
+import { Metadata } from "next";
+import type { ExplanationItem } from "./data/fileStructureExplanations";
+import { basicStructure } from "./data/fileStructure";
+import { basicExplanations } from "./data/fileStructureExplanations";
+import { FileText, Folder } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+export const metadata: Metadata = {
+	title: "Express.js Project Structure",
+	description:
+		"A scalable and maintainable folder structure for Express.js applications",
+	openGraph: {
+		title: "Express.js Project Structure",
+		description:
+			"Learn how to structure your Express.js project for scalability and maintainability.",
+		images: [
+			{
+				url: "/opengraph-image/next-js",
+				width: 1200,
+				height: 630,
+				alt: "Express.js Project Structure Overview",
+			},
+		],
+
+		url: "/express-js",
+	},
+};
+
+const BREADCRUMB_ITEMS = [
+	{ label: "Home", href: "/" },
+	{ label: "Express.js", href: "/express-js" },
+] as const;
+
+function ExplanationSection({ title, icon, description }: ExplanationItem) {
+	return (
+		<div>
+			<h3 className="mb-2 text-2xl text-neutral-950 dark:text-neutral-50">
+				<Badge>
+					{icon === "folder" ? (
+						<Folder size={16} className="mr-1" />
+					) : (
+						<FileText size={16} className="mr-1" />
+					)}
+					{title}
+				</Badge>
+			</h3>
+			<p>{description}</p>
+		</div>
+	);
+}
+
+export default function ExpressJsStructurePage() {
+	const renderExplanations = (explanations: ExplanationItem[]) => (
+		<>
+			{explanations.map((item, index) => (
+				<ExplanationSection key={index} {...item} />
+			))}
+		</>
+	);
+
+	return (
+		<>
+			<StructureTemplate
+				title="Express.js Project Structure"
+				description="A scalable and maintainable folder structure for Express.js applications"
+				breadcrumbItems={[...BREADCRUMB_ITEMS]}
+				structures={{
+					basic: basicStructure,
+					intermediate: [],
+					advanced: [],
+				}}
+				explanations={{
+					basic: renderExplanations(basicExplanations),
+					intermediate: renderExplanations([]),
+					advanced: renderExplanations([]),
+				}}
+			/>
+		</>
+	);
+}
